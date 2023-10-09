15067e1818c5bda83f5120107f383f822ce724a1
Dandara Soares <140434776+Dandafoox@users.noreply.github.com>
2023-10-09 15:35:40 -0300

    Update Aulas.java

--- Aulas.java
+++ Aulas.java
@@ -666,3 +666,172 @@ public class vetor4 {
 }
 
 
+// AULA 09/10/2023
+
+
+//controller
+
+package Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Cliente;
+
+/**
+ * Servlet implementation class ClienteController
+ */
+
+@WebServlet({"/ClienteController","/novocliente"}) // rota para chamar os outros. Tomcat nove já vem com a rota
+public class ClienteController extends HttpServlet { //chamar a classe
+	
+	
+	Cliente cli= new Cliente();
+	
+	
+	
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ClienteController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	
+	protected void EnviaDados(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { //classe
+		 cli.setIdcliente(Integer.parseInt (request.getParameter("idcliente"))); // request = requisição, Integer.paserInt para modificar letra pra numero, conversor.
+		 cli.setNome(request.getParameter("nome"));
+		 cli.setTelefone(request.getParameter("telefone"));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+
+// model
+
+
+
+package model;
+
+public class Cliente {
+	
+	private int idcliente;
+	private String nome;
+	private String telefone;
+	
+	
+	
+	public Cliente() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Cliente(int idcliente, String nome, String telefone) {
+		super();
+		this.idcliente = idcliente;
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+
+
+	public int getIdcliente() {
+		return idcliente;
+	}
+
+
+	public void setIdcliente(int idcliente) {
+		this.idcliente = idcliente;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
+
+}
+
+
+// view
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title> Cadastro de Cliente </title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+</head>
+<body>
+ <div class ="container">
+		
+		<form action = "novocliente" method ="">
+			  <div class="form-group">
+			    <label for="idcliente">Código do Cliente</label>
+			    <input type="text" class="form-control" id="" name = "idcliente">
+			  </div>
+			  
+			  <div class="form-group">
+			    <label for="nome"> Nome </label>
+			    <input type="text" class="form-control" id="" name= "nome">
+			  </div>
+			  
+			  <div class="form-group">
+			    <label for="telefone"> Telefone </label>
+			    <input type="text" class="form-control" id="" name="telefone">
+			  </div>
+			  
+			  <div class = "form group">
+			   <input type ="submit" class = "btn btn-primary" value ="Enviar">
+			  </div>
+			  
+		</form>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
