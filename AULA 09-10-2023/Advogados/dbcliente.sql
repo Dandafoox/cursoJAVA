7ad71f3fb0043946bbab2325e57cb781006331d2
Dandara Soares <140434776+Dandafoox@users.noreply.github.com>
2023-10-09 16:57:01 -0300

    AULA 09-10-23

---
+++ AULA 09-10-2023/Advogados/dbcliente.sql
@@ -0,0 +1,14 @@
+create database escritorio;
+
+use escritorio;
+
+create table cliente (
+	idcliente integer primary key auto_increment,
+	 nome varchar (60) not null,
+     telefone varchar (14) not null);
+       
+       insert into cliente(nome,telefone) 
+       values ('Dandara', '(21)98384208'),
+			('Gabriela', '(21)986498708');
+
+select * from cliente;
